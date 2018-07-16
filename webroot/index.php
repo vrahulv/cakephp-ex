8d07c34a7f169177c0aee3a2ed321628bb548cad
vrahulv <vrahulv@users.noreply.github.com>
2018-07-16 09:01:21 +0800

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -2,7 +2,7 @@
 <html>  
 <body>
 
-<form action="welcome.php" method="post">
+<form action="http://vote-my-vote-app.apps.sreejith.io/" method="get">
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
